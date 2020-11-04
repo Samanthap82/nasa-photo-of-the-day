a7aaa103f14005fd9aef954da5060cc24b387c86
Samantha Prewitt <67523846+Samanthap82@users.noreply.github.com>
2020-11-04 16:00:13 -0600

    started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
-}
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+  const [exp, setExp] = useState('');
+  const [date, setDate] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [diffDay, setDiffDay] = useState(false);
+  useEffect(()=>{
+axios.get('https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW')
+.then(res=>{
+  console.log(res)
+  setUrl(res.data.url)
+  setTitle(res.data.title);
+  setExp(res.data.explanation);
+  setDate(res.data.date)
+})
+.catch(err=>{
+  let date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW&date=${year}-${month + 1}-${day}`)
+  .then(res=>{
+    setUrl(res.data.url);
+    setTitle(res.data.title);
+    setExp(res.data.explanation);
+    setDate(res.data.date);
+    setDiffDay(true);
+  })
+});
+  }, []);
+
+
+  const changeInput = obj => {
+const value = obj.target.value;
+setInputValue(value);
+  };
+
+  const getRandUrl = () => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=YbUynbWVo92eAUAdlerdxb0JPthz9HDt5YPvb9PW&date=${inputValue}`)
+    .then(res=>{
+      console.log(res);
+      setUrl(res.data.url);
+      setTitle(res.data.title);
+      setExp(res.data.explanation);
+      setDate(res.data.date)
+      setDiffDay(true);
+    })
+    .catch(err=>{
+      alert('Please enter a different date or use the correct format!')
+    })
+  }
+
 
 export default App;
